b0dac30c29471a2ee6efe6210aea78390f8b46b6
MCopeland61 <mcopeland61@ivytech.edu>
2025-10-06 23:13:24 -0400

    module 6

---
+++ Module 6PythonCatsAndDogs/MaeveCopelandModule6PythonAssignment.py
@@ -0,0 +1,4 @@
+c = "cats" #string for cats
+d = "dogs" #string for dogs
+s = "It is raining " + c + " and " + d + "." #printable string that combines the two previously declared strings
+print(s) #prints the previous statement
